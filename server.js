b236a4c6ebbb4d88d96fbc2d69bdd1a9a6879334
Jeff Gerber <jallengerber@gmail.com>
2016-02-24 15:43:27 -0800

    Working on Nutritionix

--- server.js
+++ server.js
@@ -3,17 +3,45 @@ require('dotenv').load({silent: true}); //loads environment variables defined in
 var express = require('express');
 var app = express();
 
-process.env.APP_SECRET = process.env.APP_SECRET || 'for the love of zeus! Change Me!';
+var nutritionix = require('nutritionix')({
+    appId: process.env.NUTRITIONIX_APP_ID,
+    appKey: process.env.NUTRITIONIX_API_KEY
+}, false);
 
-app.use(express.static(__dirname + '/build'));
 
-var upcRouter = require(__dirname + '/routes/upc_routes');
+//process.env.APP_SECRET = process.env.APP_SECRET || 'for the love of zeus! Change Me!';
 
-app.use('/', upcRouter);
+//app.use(express.static(__dirname + '/build'));
 
-app.use(function(req, res) {
-  res.status(404).send('Page not found');
+//var upcRouter = require(__dirname + '/routes/upc_routes');
+// var nutritionixRouter = require(__dirname + '/routes/nutritionix_routes');
+
+// app.use('/', upcRouter);
+// app.use('/', nutritionixRouter);
+
+// app.get('/', function (req, res) {
+//   res.send('hello world');
+// });
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
 });
+
+// app.post('/search', function (req, res) {
+//   console.log(nutritionix);
+//       nutritionix.item({
+//           upc: 52200004265
+//       }, function(err, item) {
+//           if (err) {
+//               return handleError(err, res);
+//           }
+//           res.json(item);
+//       });
+// });
+// app.use(function(req, res) {
+//   res.status(404).send('Page not found');
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
