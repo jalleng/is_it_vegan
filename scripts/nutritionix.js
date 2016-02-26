94d1674a0208730ada2270e96dab1d1c13f17992
Jeff Gerber <jallengerber@gmail.com>
2016-02-25 17:00:37 -0800

    split up query string

--- scripts/nutritionix.js
+++ scripts/nutritionix.js
@@ -4,24 +4,20 @@
   var request = require('request');
   var appId = process.env.NUTRITIONIX_APP_ID;
   var appKey = process.env.NUTRITIONIX_API_KEY;
-  var urlBase = "https://api.nutritionix.com/v1_1/search/mcdonalds?";
+  var urlBase = 'https://api.nutritionix.com/v1_1/item?';
 
+  var upc = '37014242478';
 
-
-
-
-  var queryString = 'https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=' + appId + '&appKey=' + appKey ;
-
-
-
+  var queryString = urlBase +
+    'upc=' + upc +
+    '&appId=' + appId +
+    '&appKey=' + appKey;
 
   module.exports = function(query,params,cb) {
     request.get({url:queryString}, function(err,response) {
         console.log("Sweet data", response.body);
      })
   };
-
-
 
 
 
