a023a3aca55b750d401e6765da84224eb0e5e837
Jeff Gerber <jallengerber@gmail.com>
2016-07-27 17:59:42 -0700

    added cors

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ let port = process.env.PORT || 3000;
 let http = require('http');
 let express = require('express');
 let app = express();
+let cors = require('cors');
 require('dotenv').load({silent: true}); //loads environment variables defined in .env; for use in development; detects that .env exists
 let sessionId = '767b4dda-9cce-486d-abd1-2241bd93d489';
 let apiKey = process.env.FOODESSENTIALS_API_KEY;
 
+app.use(cors());
 app.use(express.static(__dirname + '/build'));
 
 app.get('/search', function(req,res) {
